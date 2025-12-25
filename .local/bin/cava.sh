3c50f3674ef7186d6e019481fa90b749e9dcbbc0
Faye <171467756+seraphicfae@users.noreply.github.com>
2025-12-24 20:17:19 -0500

    Adjusted the script and readded config-waybar

--- .local/bin/cava.sh
+++ .local/bin/cava.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 # Creator : Faye | https://github.com/seraphicfae/niri-dotfiles
 
-trap "pkill cava" EXIT
-
-cava -p /dev/stdin <<EOF | sed -u 's/;//g;s/0/ /g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g'
-[general]
-bars = 10
-[output]
-method = raw
-raw_target = /dev/stdout
-data_format = ascii
-ascii_max_range = 7
-EOF
+cava -p "$HOME/.config/cava/config-waybar" | awk -F';' '{
+    bars = ""
+    for (i = 1; i <= NF; i++) {
+        if ($i == 0) bars = bars "▁"
+        else if ($i == 1) bars = bars "▂"
+        else if ($i == 2) bars = bars "▃"
+        else if ($i == 3) bars = bars "▄"
+        else if ($i == 4) bars = bars "▅"
+        else if ($i == 5) bars = bars "▆"
+        else if ($i == 6) bars = bars "▇"
+        else if ($i == 7) bars = bars "█"
+        else bars = bars " "
+    }
+    print bars
+    fflush()
+}'
