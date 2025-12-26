1bfa5be4a8c060a1c2a518b289b0737c39d37181
Faye <171467756+seraphicfae@users.noreply.github.com>
2025-12-25 20:09:19 -0500

    Added some basic nvim settings

---
+++ .config/nvim/init.lua
@@ -0,0 +1,59 @@
+-- Basic Settings
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+
+-- Keymaps
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>e', ':Ex<CR>')
+
+-- Bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local status, ts = pcall(require, "nvim-treesitter.configs")
+            if status then
+                ts.setup({
+                    ensure_installed = { "lua", "vim", "bash", "markdown" },
+                    highlight = { enable = true },
+                })
+            end
+        end
+    },
+
+    -- File Searcher
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        end
+    },
+
+    -- Theme
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme kanagawa-dragon")
+        end
+    }
+})
